625583ba16d9d37d34cc27c2cd569646bcb59228
enricocid <enrico2588@gmail.com>
2018-02-27 14:42:23 +0100

    Simplify placeholder activity
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/musicplayergo/PlaceholderActivity.java
+++ project/app/src/main/java/com/iven/musicplayergo/PlaceholderActivity.java
@@ -28,16 +28,12 @@ import com.iven.musicplayergo.utils.AndroidVersion;
 
 public class PlaceholderActivity extends Activity {
 
-    public static final int ANIM_ITEM_DURATION = 1000;
-    public static final int ITEM_DELAY = 300;
-    public static int STARTUP_DELAY = 300;
+    private static final int ANIM_ITEM_DURATION = 1000;
+    private static final int REQUEST_CODE = 2588;
+    private static int ITEM_DELAY = 300;
     private ViewGroup mContainer;
     private TextView mRationale;
-
-    @TargetApi(23)
-    private boolean hasStoragePermission() {
-        return checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-    }
+    private Button mGrantButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +42,7 @@ public class PlaceholderActivity extends Activity {
         setContentView(R.layout.activity_placeholder);
         mContainer = findViewById(R.id.container);
         mRationale = findViewById(R.id.perm_rationale);
+        mGrantButton = findViewById(R.id.grant_button);
 
         if (AndroidVersion.isMarshmallow()) {
             animate(hasStoragePermission());
@@ -71,7 +68,7 @@ public class PlaceholderActivity extends Activity {
         ObjectAnimator a = ObjectAnimator.ofInt(mRationale, "textColor", Color.WHITE, color);
 
         a.setInterpolator(new LinearInterpolator());
-        a.setDuration(1000);
+        a.setDuration(ANIM_ITEM_DURATION);
         a.setRepeatCount(ValueAnimator.RESTART);
         a.setRepeatMode(ValueAnimator.REVERSE);
         a.setEvaluator(new ArgbEvaluator());
@@ -108,14 +105,14 @@ public class PlaceholderActivity extends Activity {
         if (hasPermission) {
             mContainer.setVisibility(View.GONE);
 
-            STARTUP_DELAY = 0;
+            ITEM_DELAY = 0;
             ViewGroup.MarginLayoutParams logoViewLayoutParams = (ViewGroup.MarginLayoutParams) logoView.getLayoutParams();
             logoViewLayoutParams.setMargins(0, -2 * translationY, 0, 0);
         }
 
         ViewCompat.animate(logoView)
                 .translationY(translationY)
-                .setStartDelay(STARTUP_DELAY)
+                .setStartDelay(ITEM_DELAY)
                 .setDuration(ANIM_ITEM_DURATION).setInterpolator(
                 new DecelerateInterpolator(1.2f)).setListener(new ViewPropertyAnimatorListener() {
             @Override
@@ -135,42 +132,36 @@ public class PlaceholderActivity extends Activity {
         }).start();
     }
 
-    @TargetApi(23)
-    private void requestReadPermission() {
-        final int READ_FILES_CODE = 2588;
-        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
-                , READ_FILES_CODE);
-    }
-
     private void animate(final boolean hasPermission) {
 
         if (!hasPermission) {
 
             animateLogo(-64, false);
-            for (int i = 0; i < mContainer.getChildCount(); i++) {
-                View v = mContainer.getChildAt(i);
-                ViewPropertyAnimatorCompat viewAnimator;
 
-                if (!(v instanceof Button) && v.equals(mRationale)) {
-                    viewAnimator = ViewCompat.animate(v)
-                            .translationY(48).alpha(1)
-                            .setStartDelay((ITEM_DELAY * i) + 500)
-                            .setDuration(1000);
-                } else {
-                    viewAnimator = ViewCompat.animate(v)
-                            .scaleY(1).scaleX(1)
-                            .setStartDelay((ITEM_DELAY * i) + 500)
-                            .setDuration(500);
-                    v.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            requestReadPermission();
-                        }
-                    });
+            ViewPropertyAnimatorCompat rationaleAnimator;
+            rationaleAnimator = ViewCompat.animate(mRationale)
+                    .translationY(48).alpha(1)
+                    .setStartDelay(ANIM_ITEM_DURATION / 2)
+                    .setDuration(ANIM_ITEM_DURATION);
+            rationaleAnimator.setInterpolator(new DecelerateInterpolator());
+
+            ViewPropertyAnimatorCompat buttonAnimator;
+            buttonAnimator = ViewCompat.animate(mGrantButton)
+                    .scaleY(1).scaleX(1)
+                    .setStartDelay(ITEM_DELAY + ANIM_ITEM_DURATION / 2)
+                    .setDuration(ANIM_ITEM_DURATION / 2);
+            buttonAnimator.setInterpolator(new DecelerateInterpolator());
+
+            rationaleAnimator.start();
+            buttonAnimator.start();
+
+            mGrantButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    requestReadPermission();
                 }
+            });
 
-                viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
-            }
         } else {
             animateLogo(-192, true);
         }
@@ -180,5 +171,16 @@ public class PlaceholderActivity extends Activity {
         Intent i = new Intent(PlaceholderActivity.this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    @TargetApi(23)
+    private boolean hasStoragePermission() {
+        return checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @TargetApi(23)
+    private void requestReadPermission() {
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
+                , REQUEST_CODE);
     }
 }
