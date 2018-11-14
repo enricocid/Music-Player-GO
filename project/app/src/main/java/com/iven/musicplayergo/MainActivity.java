db98db3b03e7d85f3211f29713be54d886237cab
enricocid <enrico2588@gmail.com>
2018-11-14 11:49:03 +0100

    Fix skip previous button reset mode
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/musicplayergo/MainActivity.java
+++ project/app/src/main/java/com/iven/musicplayergo/MainActivity.java
@@ -495,7 +495,7 @@ public class MainActivity extends AppCompatActivity implements SongsAdapter.Song
 
     private void updateResetStatus(final boolean onPlaybackCompletion) {
         final int themeColor = sThemeInverted ? R.color.white : R.color.black;
-        final int color = onPlaybackCompletion ? themeColor : mPlayerAdapter.isReset() ? themeColor : mAccent;
+        final int color = onPlaybackCompletion ? themeColor : mPlayerAdapter.isReset() ? mAccent : themeColor;
         mSkipPrevButton.post(() -> mSkipPrevButton.setColorFilter(Utils.getColorFromResource(MainActivity.this, color, onPlaybackCompletion ? themeColor : mPlayerAdapter.isReset() ? R.color.blue : themeColor), PorterDuff.Mode.SRC_IN));
     }
 
