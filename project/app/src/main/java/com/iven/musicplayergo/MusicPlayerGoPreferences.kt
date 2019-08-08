f88f07daaa9e4e39c0368dfcc7a937cc6e66b36b
TacoTheDank <SkytkRSfan3895@gmail.com>
2019-08-08 16:30:04 -0400

    Kotlin jdk7 to jdk8, fix deprecation in Preferences

--- project/app/src/main/java/com/iven/musicplayergo/MusicPlayerGoPreferences.kt
+++ project/app/src/main/java/com/iven/musicplayergo/MusicPlayerGoPreferences.kt
@@ -2,7 +2,7 @@ package com.iven.musicplayergo
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 
 class MusicPlayerGoPreferences(context: Context) {
 
