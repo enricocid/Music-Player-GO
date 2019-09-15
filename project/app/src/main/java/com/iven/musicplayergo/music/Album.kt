3580142fa900011d8aa6331e8c2d6bf6be6806e2
enricocid <enrico2588@gmail.com>
2019-09-15 14:27:59 +0200

    First UI iteration: BottomNavigationView
    BottomNavigationView with 3 buttons opening the respective fragments: Music, NowPlaying, Settings
    
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/musicplayergo/music/Album.kt
+++
@@ -1,6 +0,0 @@
-package com.iven.musicplayergo.music
-
-data class Album(
-    val title: String?,
-    val year: String?
-)
--- project/app/src/main/java/com/iven/musicplayergo/player/MediaPlayerInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.iven.musicplayergo.player
-
-interface MediaPlayerInterface {
-    fun onPositionChanged(position: Int)
-    fun onStateChanged()
-    fun onPlaybackCompleted()
-    fun onClose()
-}
