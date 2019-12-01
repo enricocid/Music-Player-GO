b690cc88ad475bdede1fd9eb0dead348162bbb15
enricocid <enrico2588@gmail.com>
2019-12-01 10:29:53 +0100

    Fix crash
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/musicplayergo/fragments/FoldersFragment.kt
+++ project/app/src/main/java/com/iven/musicplayergo/fragments/FoldersFragment.kt
@@ -3,7 +3,6 @@ package com.iven.musicplayergo.fragments
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.SearchView
