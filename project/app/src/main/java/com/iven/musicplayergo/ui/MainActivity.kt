enricocid/Music-Player-GO

commit c0b24c78f02e0985b32917bb9d84751ca93dbe4e

Fixes #178
Signed-off-by: enricocid <enrico2588@gmail.com>

project/app/src/main/java/com/iven/musicplayergo/ui/MainActivity.kt
@@ -70,7 +70,7 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
         )
 
     // Fragments
-    private var mActiveFragments: MutableList<String>? = null
+    private val mActiveFragments: List<Int> = goPreferences.activeFragments.toList()
     private var mArtistsFragment: ArtistsFoldersFragment? = null
     private var mAllMusicFragment: AllMusicFragment? = null
     private var mFoldersFragment: ArtistsFoldersFragment? = null
@@ -233,8 +233,6 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
 
         initMediaButtons()
 
-        mActiveFragments = goPreferences.activeFragments?.toMutableList()
-
         sRestoreSettingsFragment =
             savedInstanceState?.getBoolean(GoConstants.RESTORE_SETTINGS_FRAGMENT)
                 ?: intent.getBooleanExtra(
@@ -268,7 +266,6 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
                     alpha(1.0F)
                 }
             }
-
         } else {
             notifyError(GoConstants.TAG_NO_MUSIC)
         }
@@ -284,7 +281,7 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
     private fun initViewPager() {
 
         val pagerAdapter = ScreenSlidePagerAdapter(this)
-        mMainActivityBinding.viewPager2.offscreenPageLimit = mActiveFragments?.size?.minus(1)!!
+        mMainActivityBinding.viewPager2.offscreenPageLimit = mActiveFragments.size.minus(1)
         mMainActivityBinding.viewPager2.adapter = pagerAdapter
 
         mPlayerControlsPanelBinding.tabLayout.apply {
@@ -292,10 +289,9 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
             tabIconTint = ColorStateList.valueOf(mResolvedAlphaAccentColor)
 
             TabLayoutMediator(this, mMainActivityBinding.viewPager2) { tab, position ->
-                mActiveFragments?.get(position)?.toInt()?.let { currentFragmentIndex ->
-                    tab.setIcon(ThemeHelper.getTabIcon(currentFragmentIndex))
-                    initFragmentAtIndex(currentFragmentIndex)
-                }
+                val fragmentIndex = mActiveFragments[position]
+                tab.setIcon(ThemeHelper.getTabIcon(fragmentIndex))
+                initFragmentAtPosition(fragmentIndex)
             }.attach()
 
             addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
@@ -325,8 +321,8 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
         )
     }
 
-    private fun initFragmentAtIndex(index: Int) {
-        when (index) {
+    private fun initFragmentAtPosition(fragmentIndex: Int) {
+        when (fragmentIndex) {
             0 -> if (mArtistsFragment == null) mArtistsFragment =
                 ArtistsFoldersFragment.newInstance(GoConstants.TAG_ARTISTS)
             1 -> if (mAllMusicFragment == null) mAllMusicFragment = AllMusicFragment.newInstance()
@@ -337,14 +333,7 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
         }
     }
 
-    private fun handleOnNavigationItemSelected(itemId: Int) = when (itemId) {
-        0 -> getFragmentForIndex(mActiveFragments?.get(0)?.toInt())
-        1 -> getFragmentForIndex(mActiveFragments?.get(1)?.toInt())
-        2 -> getFragmentForIndex(mActiveFragments?.get(2)?.toInt())
-        else -> getFragmentForIndex(mActiveFragments?.get(3)?.toInt())
-    }
-
-    private fun getFragmentForIndex(index: Int?) = when (index) {
+    private fun getFragmentForIndex(index: Int) = when (index) {
         0 -> mArtistsFragment
         1 -> mAllMusicFragment
         2 -> mFoldersFragment
@@ -1010,9 +999,9 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
 
     // ViewPager2 adapter class
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-        override fun getItemCount(): Int = mActiveFragments?.size!!
+        override fun getItemCount(): Int = mActiveFragments.size
 
         override fun createFragment(position: Int): Fragment =
-            handleOnNavigationItemSelected(position)!!
+            getFragmentForIndex(position)!!
     }
 }

project/app/src/main/java/com/iven/musicplayergo/GoPreferences.kt
@@ -20,8 +20,7 @@ class GoPreferences(context: Context) {
     private val prefsEdgeToEdge = context.getString(R.string.edge_pref)
 
     private val prefsActiveFragments = context.getString(R.string.active_fragments_pref)
-    private val prefsActiveFragmentsDefault =
-        context.resources.getStringArray(R.array.activeFragmentsEntryArray).toMutableSet()
+    val prefsActiveFragmentsDefault = setOf(0, 1, 2, 3)
 
     private val prefsArtistsSorting = context.getString(R.string.artists_sorting_pref)
     private val prefsFoldersSorting = context.getString(R.string.folders_sorting_pref)
@@ -34,6 +33,9 @@ class GoPreferences(context: Context) {
 
     private val mPrefs = PreferenceManager.getDefaultSharedPreferences(context)
     private val mGson = GsonBuilder().create()
+
+    // active fragments type
+    private val typeActiveFragments = object : TypeToken<Set<Int>>() {}.type
 
     // last played song is a SavedMusic
     private val typeLastPlayedSong = object : TypeToken<SavedMusic>() {}.type
@@ -74,9 +76,9 @@ class GoPreferences(context: Context) {
         ) && VersioningHelper.isOreoMR1()
         set(value) = mPrefs.edit().putBoolean(prefsEdgeToEdge, value).apply()
 
-    var activeFragments: Set<String>?
-        get() = mPrefs.getStringSet(prefsActiveFragments, prefsActiveFragmentsDefault)
-        set(value) = mPrefs.edit().putStringSet(prefsActiveFragments, value).apply()
+    var activeFragments: Set<Int>
+        get() = getObject(prefsActiveFragments, typeActiveFragments) ?: prefsActiveFragmentsDefault
+        set(value) = putObject(prefsActiveFragments, value)
 
     var artistsSorting
         get() = mPrefs.getInt(prefsArtistsSorting, GoConstants.DESCENDING_SORTING)

project/app/src/main/java/com/iven/musicplayergo/adapters/ActiveTabsAdapter.kt
@@ -18,15 +18,13 @@ class ActiveTabsAdapter(
 ) :
     RecyclerView.Adapter<ActiveTabsAdapter.CheckableItemsHolder>() {
 
-    private val mItemsToRemove = mutableListOf<String>()
+    private val mAvailableItems = goPreferences.prefsActiveFragmentsDefault
+    private val mActiveItems = goPreferences.activeFragments.toMutableList()
 
-    private val mAvailableItems =
-        context.resources.getStringArray(R.array.activeFragmentsListArray).toMutableList()
-    private val mActiveItems = goPreferences.activeFragments?.toMutableList()
-
-    fun getUpdatedItems(): Set<String>? {
-        mActiveItems?.removeAll(mItemsToRemove.toSet())
-        return mActiveItems?.toSet()
+    fun getUpdatedItems(): Set<Int> {
+        // make sure to respect tabs order
+        mActiveItems.sortBy { it }
+        return mActiveItems.toSet()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheckableItemsHolder {
@@ -60,7 +58,7 @@ class ActiveTabsAdapter(
 
                 if (isEnabled) {
                     manageIndicatorsStatus(
-                        mActiveItems?.contains(adapterPosition.toString())!!,
+                        mActiveItems.contains(adapterPosition),
                         tabImageButton,
                         indicator
                     )
@@ -83,15 +81,13 @@ class ActiveTabsAdapter(
                         indicator
                     )
 
-                    if (indicator.visibility != View.VISIBLE) mActiveItems?.remove(
-                        adapterPosition.toString()
-                    ) else mActiveItems?.add(
-                        adapterPosition.toString()
-                    )
-                    if (mActiveItems?.size!! < 2) {
+                    if (indicator.visibility != View.VISIBLE) mActiveItems.remove(
+                        adapterPosition
+                    ) else mActiveItems.add(adapterPosition)
+                    if (mActiveItems.size < 2) {
                         context.getString(R.string.active_fragments_pref_warning)
                             .toToast(context)
-                        mActiveItems.add(adapterPosition.toString())
+                        mActiveItems.add(adapterPosition)
                         manageIndicatorsStatus(true, tabImageButton, indicator)
                     }
                 }

project/app/src/main/java/com/iven/musicplayergo/fragments/PreferencesFragment.kt
@@ -107,7 +107,7 @@ class PreferencesFragment : PreferenceFragmentCompat(),
         }
 
         findPreference<Preference>(getString(R.string.active_fragments_pref))?.apply {
-            summary = goPreferences.activeFragments?.size.toString()
+            summary = goPreferences.activeFragments.size.toString()
             onPreferenceClickListener = this@PreferencesFragment
         }
     }

project/app/src/main/res/values/arrays.xml
@@ -13,18 +13,4 @@
         <item>@string/theme_pref_dark</item>
         <item>@string/theme_pref_auto</item>
     </string-array>
-
-    <string-array name="activeFragmentsListArray">
-        <item>@string/active_frag_artist</item>
-        <item>@string/active_frag_music</item>
-        <item>@string/active_frag_folders</item>
-        <item>@string/active_frag_settings</item>
-    </string-array>
-
-    <string-array name="activeFragmentsEntryArray">
-        <item>0</item>
-        <item>1</item>
-        <item>2</item>
-        <item>3</item>
-    </string-array>
 </resources>

project/app/src/main/res/values/donottranslate.xml
@@ -42,7 +42,7 @@
     <string name="theme_pref_auto">theme_pref_auto</string>
     <string name="accent_pref">accent_pref</string>
     <string name="edge_pref">edge_pref</string>
-    <string name="active_fragments_pref">selected_fragments_pref</string>
+    <string name="active_fragments_pref">enabled_fragments_pref</string>
     <string name="active_frag_artist">active_frag_artist</string>
     <string name="active_frag_music">active_frag_music</string>
     <string name="active_frag_folders">active_frag_folder</string>

commit 80cceb891011501262229547cf2e832d54724932

Fix crash introduced in c0b24c7
Signed-off-by: enricocid <enrico2588@gmail.com>

project/app/src/main/java/com/iven/musicplayergo/ui/MainActivity.kt
@@ -337,6 +337,13 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
         }
     }
 
+    private fun handleOnNavigationItemSelected(itemId: Int) = when (itemId) {
+        0 -> getFragmentForIndex(mActiveFragments[0])
+        1 -> getFragmentForIndex(mActiveFragments[1])
+        2 -> getFragmentForIndex(mActiveFragments[2])
+        else -> getFragmentForIndex(mActiveFragments[3])
+    }
+
     private fun getFragmentForIndex(index: Int) = when (index) {
         0 -> mArtistsFragment
         1 -> mAllMusicFragment
@@ -1006,6 +1013,6 @@ class MainActivity : AppCompatActivity(), UIControlInterface {
         override fun getItemCount(): Int = mActiveFragments.size
 
         override fun createFragment(position: Int): Fragment =
-            getFragmentForIndex(position)!!
+            handleOnNavigationItemSelected(position)!!
     }
 }