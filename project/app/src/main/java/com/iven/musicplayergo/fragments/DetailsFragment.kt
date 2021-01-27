a80139097b82d11f6752b1c2e3769b07c59af376
enricocid <enrico2588@gmail.com>
2021-01-27 17:46:23 +0100

    Details fragment: fix wrong song played when double click albums
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/musicplayergo/fragments/DetailsFragment.kt
+++ project/app/src/main/java/com/iven/musicplayergo/fragments/DetailsFragment.kt
@@ -674,9 +674,9 @@ class DetailsFragment : Fragment(R.layout.fragment_details), SearchView.OnQueryT
                         } else {
                             if (sPlayFirstSong) {
                                 mUIControlInterface.onSongSelected(
-                                        item.music?.get(0),
-                                        item.music,
-                                        mLaunchedBy
+                                    ListsHelper.getSortedMusicList(mSongsSorting, item.music?.toMutableList())?.get(0),
+                                    item.music,
+                                    mLaunchedBy
                                 )
                             } else {
                                 sPlayFirstSong = true
