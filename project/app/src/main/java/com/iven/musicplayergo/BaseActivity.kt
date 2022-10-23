5845b3bcc2840a598a57d3ccec7f405f50865b09
enricocid <enrico2588@gmail.com>
2022-10-23 21:15:00 +0200

    Equalizer (black theme): fix sb color

--- project/app/src/main/java/com/iven/musicplayergo/BaseActivity.kt
+++ project/app/src/main/java/com/iven/musicplayergo/BaseActivity.kt
@@ -1,12 +1,21 @@
 package com.iven.musicplayergo
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import com.iven.musicplayergo.preferences.ContextUtils
+import com.iven.musicplayergo.utils.Theming
 import java.util.*
 
 
 abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        if (Theming.isThemeBlack(resources)) {
+            window?.statusBarColor = Color.BLACK
+        }
+    }
 
     override fun attachBaseContext(newBase: Context?) {
         newBase?.let { ctx ->
