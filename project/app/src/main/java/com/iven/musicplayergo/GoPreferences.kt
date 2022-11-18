c6ab023340f4b8c2ec99ff15d5c141e1782aeae4
enricocid <enrico2588@gmail.com>
2022-11-18 22:50:04 +0100

    Improve preferences

--- project/app/src/main/java/com/iven/musicplayergo/GoPreferences.kt
+++ project/app/src/main/java/com/iven/musicplayergo/GoPreferences.kt
@@ -6,6 +6,7 @@ import androidx.preference.PreferenceManager
 import com.iven.musicplayergo.models.Music
 import com.iven.musicplayergo.models.NotificationAction
 import com.iven.musicplayergo.models.SavedEqualizerSettings
+import com.iven.musicplayergo.models.Sorting
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import java.lang.reflect.Type
@@ -13,49 +14,50 @@ import java.lang.reflect.Type
 
 class GoPreferences(context: Context) {
 
-    private val prefsSavedEqualizerSettings = context.getString(R.string.saved_eq_settings)
-    private val prefsLatestVolume = context.getString(R.string.latest_volume_pref)
-    private val prefsLatestPlaybackVel = context.getString(R.string.latest_playback_vel_pref)
-    private val prefsLatestPlayedSong = context.getString(R.string.latest_played_song_pref)
-    private val prefsFavorites = context.getString(R.string.favorites_pref)
-    private val prefsQueue = context.getString(R.string.queue_pref)
-    private val prefsIsQueue = context.getString(R.string.is_queue_pref)
+    private val prefsSavedEqualizerSettings = "eq_settings_pref"
+    private val prefsLatestVolume = "latest_volume_pref"
+    private val prefsLatestPlaybackVel = "latest_playback_vel_pref"
+    private val prefsLatestPlayedSong = "to_restore_song_pref"
+    private val prefsFavorites = "favorite_songs_pref"
+    private val prefsQueue = "queue_songs_pref"
+    private val prefsIsQueue = "is_queue_pref"
 
-    private val prefsTheme = context.getString(R.string.theme_pref)
-    private val prefsThemeDef = context.getString(R.string.theme_pref_auto)
-    private val prefsThemeBlack = context.getString(R.string.theme_pref_black)
-    private val prefsAccent = context.getString(R.string.accent_pref)
+    private val prefsTheme = "theme_pref"
+    private val prefsThemeDef = "theme_pref_auto"
+    private val prefsThemeBlack = "theme_pref_black"
+    private val prefsAccent = "color_primary_pref"
 
-    private val prefsActiveTabsDef = context.getString(R.string.active_tabs_def_pref)
-    private val prefsActiveTabs = context.getString(R.string.active_tabs_pref)
+    private val prefsActiveTabsDef = "active_tabs_def_pref"
+    private val prefsActiveTabs = "active_tabs_pref"
 
-    private val prefsCover = context.getString(R.string.covers_pref)
+    private val prefsCover = "covers_pref"
 
-    private val prefsOnListEnded = context.getString(R.string.on_list_ended_pref)
+    private val prefsOnListEnded = "on_list_ended_pref"
 
-    private val prefsSongsVisual = context.getString(R.string.song_visual_pref)
+    private val prefsSongsVisual = "song_visual_pref"
 
-    private val prefsArtistsSorting = context.getString(R.string.artists_sorting_pref)
-    private val prefsFoldersSorting = context.getString(R.string.folders_sorting_pref)
-    private val prefsAlbumsSorting = context.getString(R.string.albums_sorting_pref)
-    private val prefsAllMusicSorting = context.getString(R.string.all_music_sorting_pref)
+    private val prefsArtistsSorting = "sorting_artists_pref"
+    private val prefsFoldersSorting = "sorting_folder_details_pref"
+    private val prefsAlbumsSorting = "sorting_album_details_pref"
+    private val prefsAllMusicSorting = "sorting_all_music_tab_pref"
 
-    private val prefsFastSeek = context.getString(R.string.fast_seeking_pref)
-    private val prefsNotificationActions = context.getString(R.string.notif_actions_pref)
-    private val prefsEq = context.getString(R.string.eq_pref)
-    private val prefsPreciseVolume = context.getString(R.string.precise_volume_pref)
-    private val prefsFocus = context.getString(R.string.focus_pref)
-    private val prefsHeadsetPlug = context.getString(R.string.headset_pref)
+    private val prefsFastSeek = "fast_seeking_pref"
+    private val prefsNotificationActions = "notification_actions_pref"
+    private val prefsEq = "eq_pref"
+    private val prefsPreciseVolume = "precise_volume_pref"
+    private val prefsFocus = "focus_pref"
+    private val prefsHeadsetPlug = "headsets_pref"
 
-    private val prefsAnim = context.getString(R.string.anim_pref)
-    private val prefsFilter = context.getString(R.string.filter_pref)
-    private val prefsPlaybackVel = context.getString(R.string.playback_vel_pref)
-    private val prefsIsContinueOnEnd = context.getString(R.string.continue_on_end_pref)
-    private val prefsHasCompletedPlayback = context.getString(R.string.has_completed_playback_pref)
-    private val prefsIsAskConfirmation = context.getString(R.string.ask_confirmation_pref)
-    private val prefsLockRotation = context.getString(R.string.rotation_pref)
+    private val prefsAnim = "anim_pref"
+    private val prefsFilter = "strings_filter_pref"
+    private val prefsPlaybackVel = "playback_vel_pref"
+    private val prefsIsContinueOnEnd = "continue_on_end_pref"
+    private val prefsHasCompletedPlayback = "has_completed_playback_pref"
+    private val prefsIsAskConfirmation = "ask_confirmation_pref"
+    private val prefsLockRotation = "rotation_pref"
 
-    private val prefsLocale = context.getString(R.string.locale_pref)
+    private val prefsDetailsSorting = "details_sorting_pref"
+    private val prefsLocale = "locale_pref"
 
     private val mPrefs = PreferenceManager.getDefaultSharedPreferences(context)
 
@@ -67,6 +69,9 @@ class GoPreferences(context: Context) {
     // favorites is a list of Music
     private val typeFavorites = Types.newParameterizedType(List::class.java, Music::class.java)
 
+    // sortings is a list of Sorting
+    private val typeSorting = Types.newParameterizedType(List::class.java, Sorting::class.java)
+
     var latestVolume: Int
         get() = mPrefs.getInt(prefsLatestVolume, 100)
         set(value) = mPrefs.edit { putInt(prefsLatestVolume, value) }
@@ -76,10 +81,7 @@ class GoPreferences(context: Context) {
         set(value) = mPrefs.edit { putFloat(prefsLatestPlaybackVel, value) }
 
     var latestPlayedSong: Music?
-        get() = getObjectForClass(
-            prefsLatestPlayedSong,
-            Music::class.java
-        )
+        get() = getObjectForClass(prefsLatestPlayedSong, Music::class.java)
         set(value) = putObjectForClass(prefsLatestPlayedSong, value, Music::class.java)
 
     var savedEqualizerSettings: SavedEqualizerSettings?
@@ -94,24 +96,15 @@ class GoPreferences(context: Context) {
         )
 
     var favorites: List<Music>?
-        get() = getObjectForType(
-            prefsFavorites,
-            typeFavorites
-        )
+        get() = getObjectForType(prefsFavorites, typeFavorites)
         set(value) = putObjectForType(prefsFavorites, value, typeFavorites)
 
     var queue: List<Music>?
-        get() = getObjectForType(
-            prefsQueue,
-            typeFavorites
-        )
+        get() = getObjectForType(prefsQueue, typeFavorites)
         set(value) = putObjectForType(prefsQueue, value, typeFavorites)
 
     var isQueue: Music?
-        get() = getObjectForClass(
-            prefsIsQueue,
-            Music::class.java
-        )
+        get() = getObjectForClass(prefsIsQueue, Music::class.java)
         set(value) = putObjectForClass(prefsIsQueue, value, Music::class.java)
 
     var theme
@@ -220,6 +213,10 @@ class GoPreferences(context: Context) {
     var lockRotation: Boolean
         get() = mPrefs.getBoolean(prefsLockRotation, false)
         set(value) = mPrefs.edit { putBoolean(prefsLockRotation, value) }
+
+    var sortings: List<Sorting>?
+        get() = getObjectForType(prefsDetailsSorting, typeSorting)
+        set(value) = putObjectForType(prefsDetailsSorting, value, typeSorting)
 
     // Retrieve object from the Preferences using Moshi
     private fun <T : Any> putObjectForType(key: String, value: T?, type: Type) {
